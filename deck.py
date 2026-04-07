a67ee45a22a1dc26aa99eacbfc03531c04803b32
PoloCancha <pabscampana@gmail.com>
2026-04-07 18:13:03 +0200

    Poker exercise

--- deck.py
+++ deck.py
@@ -1,24 +1,29 @@
 import random
+
 class PlayingCard:
-    SUITS = ("♣", "♦", "♥", "♠")
-    RANKS = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+    SUITS = ["♥","♦","♣", "♠"]
+    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     def __init__(self, suit, rank):
-       # check if the suit and rank are ok
+        # check if the suit and rank are ok
         if suit not in self.SUITS:
-            raise ValueError(f"suit must be in {self.SUITS}")
+            raise ValueError(f"Suit must be in {self.SUITS}")
         if rank not in self.RANKS:
-            raise ValueError(f"rank must be in {self.RANKS}")
+            raise ValueError(f"Rank must be in {self.RANKS}")
         self._suit = suit
         self._rank = rank
+
     @property
-    def suit(self):
+    def suit(self): # getter, no setter!
         return self._suit
     @property
-    def rank(self):
+    def rank(self): # getter, no setter!
         return self._rank
 
+    def __gt__(self, other):
+        return PlayingCard.RANKS.index(self.rank) > PlayingCard.RANKS.index(other.rank)
+
     def __str__(self):
-        return f"{self.rank}{self.suit}" #so i can print the card
+        return f"{self.rank}{self.suit}" # so I can print the card!
     def __repr__(self):
         return self.__str__()
 
@@ -30,7 +35,7 @@ class Deck:
                 self._cards.append(PlayingCard(suit, rank))
     @property
     def cards(self):
-        return self._cards[:]
+        return self._cards[:] # anti cheat system!
     def __str__(self):
         return str(self.cards)
 
@@ -41,16 +46,16 @@ class Deck:
         return self._cards.pop(0)
 
 if __name__ == "__main__":
-    # card = PlayingCard("♦", "7")
+    card = PlayingCard("♦", "7")
     print(card)
     # card.rank = "A" # cheater, we dont like cheaters
     # print(card)
     deck = Deck()
     print(deck)
-    # deck.cards.append(card)
-    # print(deck)
+    deck.cards.append(card) # cheater found another loophole!
+    print(deck)
     deck.shuffle()
     print(deck)
-    card = deck.deal()
-    print(card)
-    print(deck)
+    card = deck.deal() # deling one card
+    print(card) # printing the card
+    print(deck) # printing the remaining deck
--- poker.py
+++ poker.py
@@ -1,4 +1,7 @@
-from deck import Deck
+from operator import truediv
+from wsgiref.util import request_uri
+
+from deck import Deck, PlayingCard
 
 class PokerHand:
     def __init__(self):
@@ -18,5 +21,86 @@ class PokerHand:
     def __str__(self):
         return str(self.cards)
 
-hand = PokerHand()
-print(hand)
+    @property
+    def is_flush(self):
+        """
+        Check if the hand is a flush
+        :return: True or False
+        """
+        for card in self.cards[1:]:
+            if self.cards[0].suit != card.suit:
+                return False
+        return True
+
+    @property
+    def is_straight_flush(self):
+        return self.is_flush and self.is_straight
+
+    @property
+    def is_normal_flush(self):
+        return self.is_flush and not self.is_straight
+
+    @property
+    def num_matches(self):
+        count = 0
+        for i in range(5):
+            for j in range(5):
+                if i == j:
+                    continue
+                if self._cards[i].rank == self._cards[j].rank:
+                    count += 1
+        return count
+
+    @property
+    def is_pair(self):
+        return self.num_matches == 2
+
+    @property
+    def is_2_pair(self):
+        return self.num_matches == 4
+
+    @property
+    def is_trips(self):
+        return self.num_matches == 6
+
+    @property
+    def is_fullhouse(self):
+        return self.num_matches == 8
+
+    @property
+    def is_4_kind(self):
+        return self.num_matches == 12
+
+    @property
+    def is_straight(self):
+        if self.num_matches == 0:
+            return False
+        cards = list(self.cards)
+        cards.sort()
+        first_card_index = PlayingCard.RANKS.index(cards[0].rank)
+        last_card_index = PlayingCard.RANKS.index(cards[-1].rank)
+        if first_card_index + 4 == last_card_index:
+            return True
+        # bicycle
+        if cards[-1].rank == "A" and cards[-2].rank == "5":
+            return True
+        return False
+
+# hand = PokerHand()
+# print(hand)
+# cards = list(hand.cards)
+# cards.sort()
+# print(cards)
+
+tries = 0
+total_hits = 100
+hits = 0
+while True:
+    hand = PokerHand()
+    tries += 1
+    if hand.is_straight_flush:
+        hits = hits + 1
+    if hits == total_hits:
+        print(f"Probability of a straight flush is {hits/tries*100}%")
+        break
+
